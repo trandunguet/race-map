a43db1d081280ecfd015d77d96cd57c8299117d5
Tran Anh Dung <trandunguet@gmail.com>
2019-05-24 08:32:04 +0700

    fix infinite bfs

--- path-find.py
+++ path-find.py
@@ -6,6 +6,8 @@ import cv2 as cv
 
 from path import Dot, Path
 
+debug = False
+
 path = Path()
 path.load()
 
@@ -19,26 +21,36 @@ def bfs(prev_start, start, finish):
         dot.reset()
     queue = Queue()
     queue.put(start.get_clone(prev_start))
+    result = []
 
     while not queue.empty():
         current = queue.get()
-        # for debugging:
-        # cv.circle(map_view, (current.x, current.y), 4, (255, 0, 0), -1)
-        # cv.imshow('map', map_view)
-        # cv.waitKey()
+
+        if debug:
+            cv.circle(map_view, (current.x, current.y), 4, (255, 0, 0), -1)
+            cv.imshow('map', map_view)
+            cv.waitKey()
 
         if current.id == finish.id:
-            return current
+            result.append(current)
             
         for adj_id in current.adj:
             adj = path.dots[adj_id]
+
+            if debug:
+                cv.circle(map_view, (adj.x, adj.y), 4, (0, 0, 255), -1)
+                cv.imshow('map', map_view)
+                cv.waitKey()
+
             if adj.prev_mark[current.id]:
                 continue
-            adj.prev_mark[current] = True
             if angle(current.prev, current, adj) > math.pi / 180 * 72:
                 continue
+            adj.prev_mark[current.id] = True
 
             queue.put(adj.get_clone(current))
+
+    return result
 
 map_view = cv.imread('roadmask.png')
 
@@ -50,7 +62,8 @@ for line in input_file:
 start = path.dots[path.checkpoints[5]]
 prev_start = path.dots[108]
 for checkpoint in checkpoints[1:]:
-    track = bfs(prev_start, start,  path.dots[path.checkpoints[checkpoint]])
+    track = bfs(prev_start, start,  path.dots[path.checkpoints[checkpoint]])[0]
+    # print(track)
     if not track:
         print("No path found!")
         exit()
